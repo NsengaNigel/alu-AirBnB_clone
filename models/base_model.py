8c709b3dee274eee78fb7ab383b986be843d7d54
NsengaNigel <nsenganigel@gmail.com>
2024-09-12 00:29:31 -0700

    Add all

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""BaseModel class module - defines all common attributes/methods for other classes"""
-
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    """A base class for all models, with common attributes and methods"""
-
+    """Base class that defines all common attributes and methods for other classes."""
+    
     def __init__(self):
-        """Initialize a new instance of BaseModel"""
-        self.id = str(uuid.uuid4())  # Generate a unique id
-        self.created_at = datetime.now()  # Set creation date and time
-        self.updated_at = self.created_at  # Set update date and time
+        """Initializes a new instance with a unique ID and timestamps."""
+        self.id = str(uuid.uuid4())  # Generate a unique ID
+        self.created_at = datetime.now()  # Set the current time
+        self.updated_at = datetime.now()  # Set the current time
 
     def __str__(self):
-        """Return string representation of the BaseModel instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Returns a string representation of the instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the 'updated_at' attribute with the current datetime"""
+        """Updates the `updated_at` attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary containing all instance attributes"""
-        obj_dict = self.__dict__.copy()  # Copy the instance's __dict__
-        obj_dict['__class__'] = self.__class__.__name__  # Add class name to the dictionary
-        obj_dict['created_at'] = self.created_at.isoformat()  # Convert datetime to ISO format
-        obj_dict['updated_at'] = self.updated_at.isoformat()  # Convert datetime to ISO format
-        return obj_dict
+        """Returns a dictionary containing all keys/values of the instance's __dict__."""
+        dict_rep = self.__dict__.copy()  # Make a copy of the __dict__
+        dict_rep["__class__"] = self.__class__.__name__  # Add the class name
+        dict_rep["created_at"] = self.created_at.isoformat()  # Convert to ISO format
+        dict_rep["updated_at"] = self.updated_at.isoformat()  # Convert to ISO format
+        return dict_rep
+
 
