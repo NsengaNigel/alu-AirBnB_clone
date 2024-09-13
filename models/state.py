6adb5ee3f55bfdacb9c344b43f4d455d421face1
NsengaNigel <nsenganigel@gmail.com>
2024-09-13 08:56:10 -0700

    nothing changed

---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for State class that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """Class representing a State"""
+    name = ""
